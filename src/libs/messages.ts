584cc1275414a9b7da59be60722d832de5bbc6b5
BiaGrenzel <biancagrenzel@gmail.com>
2024-05-26 00:09:01 -0300

    :sparkles: feat: translation

---
+++ src/libs/messages.ts
@@ -0,0 +1,241 @@
+interface Messages {
+  [key: string]: {
+    en: string;
+    pt: string;
+  };
+}
+
+export const messages: Messages = {
+  english: {
+    en: "English",
+    pt: "Inglês",
+  },
+  portuguese: {
+    en: "Portuguese",
+    pt: "Português",
+  },
+  home: {
+    en: "Home",
+    pt: "Início",
+  },
+  profile: {
+    en: "Profile",
+    pt: "Perfil",
+  },
+  exit: {
+    en: "Exit",
+    pt: "Sair",
+  },
+  email: {
+    en: "Email",
+    pt: "Email",
+  },
+  password: {
+    en: "Password",
+    pt: "Senha",
+  },
+  signIn: {
+    en: "Sign In",
+    pt: "Entrar",
+  },
+  signUp: {
+    en: "Sign Up",
+    pt: "Cadastre-se",
+  },
+  dontHaveAnAccount: {
+    en: "Don't have an account?",
+    pt: "Não tem uma conta?",
+  },
+  save: {
+    en: "Save",
+    pt: "Salvar",
+  },
+  loading: {
+    en: "Loading...",
+    pt: "Carregando...",
+  },
+  loginSuccess: {
+    en: "Login successful!",
+    pt: "Login realizado com sucesso!",
+  },
+  loginError: {
+    en: "Error logging in!",
+    pt: "Erro ao realizar login!",
+  },
+  emailInputError: {
+    en: "Invalid email",
+    pt: "Email inválido",
+  },
+  passwordInputError: {
+    en: "Verify your credentials",
+    pt: "Verifique suas credenciais",
+  },
+  register: {
+    en: "Sign Up",
+    pt: "Cadastro",
+  },
+  name: {
+    en: "Name",
+    pt: "Nome",
+  },
+  birthDate: {
+    en: "Birth Date",
+    pt: "Data de Nascimento",
+  },
+  phone: {
+    en: "Phone Number",
+    pt: "Número de Telefone",
+  },
+  haveAnAccount: {
+    en: "Already have an account?",
+    pt: "Já tem uma conta?",
+  },
+  doSignIn: {
+    en: "Sign In",
+    pt: "Faça Login",
+  },
+  userEditedSuccess: {
+    en: "User edited successfully!",
+    pt: "Usuário editado com sucesso!",
+  },
+  userEditedError: {
+    en: "Error editing user!",
+    pt: "Erro ao editar usuário!",
+  },
+  emailAlreadyInUse: {
+    en: "Email already in use",
+    pt: "Email já em uso",
+  },
+  phoneAlreadyInUse: {
+    en: "Phone already in use",
+    pt: "Telefone já em uso",
+  },
+  myProfile: {
+    en: "My Profile",
+    pt: "Meu Perfil",
+  },
+  changePassword: {
+    en: "Change Password",
+    pt: "Alterar Senha",
+  },
+  newPassword: {
+    en: "New Password",
+    pt: "Nova Senha",
+  },
+  oldPassword: {
+    en: "Old Password",
+    pt: "Senha Antiga",
+  },
+  recoverPassword: {
+    en: "Recover Password",
+    pt: "Recuperar Senha",
+  },
+  send: {
+    en: "Send",
+    pt: "Enviar",
+  },
+  forgotYourPassword: {
+    en: "Forgot your password?",
+    pt: "Esqueceu sua senha?",
+  },
+  recoverPasswordEmailSentSuccess: {
+    en: "Recover password email sent successfully!",
+    pt: "Email de recuperação de senha enviado com sucesso!",
+  },
+  recoverPasswordEmailSentError: {
+    en: "Error sending recover password email!",
+    pt: "Erro ao enviar email de recuperação de senha!",
+  },
+  requiredField: {
+    en: "Required field",
+    pt: "Campo obrigatório",
+  },
+  minSixCharacters: {
+    en: "Minimum of 6 characters",
+    pt: "Mínimo de 6 caracteres",
+  },
+  invalidEmail: {
+    en: "Invalid email",
+    pt: "Email inválido",
+  },
+  whatWillYouDo: {
+    en: "what will you do today?",
+    pt: "o que você vai fazer hoje?",
+  },
+  chooseWhatToDo: {
+    en: "Choose what to do below and let's make something amazing together!",
+    pt: "Escolha o que fazer abaixo e vamos fazer algo incrível juntos!",
+  },
+  createATask: {
+    en: "Create a task",
+    pt: "Criar uma tarefa",
+  },
+  pomodoroTimer: {
+    en: "Pomodoro Timer",
+    pt: "Temporizador Pomodoro",
+  },
+  createFeedback: {
+    en: "Create feedback",
+    pt: "Criar feedback",
+  },
+  recordWorkedHours: {
+    en: "Record worked hours",
+    pt: "Registrar horas trabalhadas",
+  },
+  createTaskDescription: {
+    en: "Create a new task for your project",
+    pt: "Crie uma nova tarefa para seu projeto",
+  },
+  pomodoroTimerDescription: {
+    en: "Start a pomodoro timer to focus on your task",
+    pt: "Inicie um temporizador pomodoro para focar em sua tarefa",
+  },
+  createFeedbackDescription: {
+    en: "Create a feedback for your team or project",
+    pt: "Crie um feedback para a sua equipe ou projeto",
+  },
+  recordWorkedHoursDescription: {
+    en: "Record the hours you worked on your project",
+    pt: "Registre as horas que você trabalhou em seu projeto",
+  },
+  todoList: {
+    en: "Todo List",
+    pt: "Lista de Tarefas",
+  },
+  createTaskSuccess: {
+    en: "Task created successfully!",
+    pt: "Tarefa criada com sucesso!",
+  },
+  createTaskError: {
+    en: "Error creating task!",
+    pt: "Erro ao criar tarefa!",
+  },
+  updateTaskSucess: {
+    en: "Task updated successfully!",
+    pt: "Tarefa atualizada com sucesso!",
+  },
+  titleInputError: {
+    en: "Title is required",
+    pt: "Título é obrigatório",
+  },
+  descriptionInputError: {
+    en: "Description is required",
+    pt: "Descrição é obrigatória",
+  },
+  createTask: {
+    en: "Create Task",
+    pt: "Criar Tarefa",
+  },
+  title: {
+    en: "Title",
+    pt: "Título",
+  },
+  description: {
+    en: "Description",
+    pt: "Descrição",
+  },
+  newTask:{
+    en: "New Task",
+    pt: "Nova Tarefa",
+  }
+};
