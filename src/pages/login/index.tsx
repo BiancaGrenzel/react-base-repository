84b88fbb83c63807dd0ac1a3d81711623f0f81a9
BiaGrenzel <biancagrenzel@gmail.com>
2024-05-26 00:11:23 -0300

    :sparkles: feat: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,109 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login: React.FC = () => {
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { Field, Form, Formik, FormikHelpers } from "formik";
+
+import { toast } from "react-toastify";
+import { useStore } from "zustand";
+import { useAuth } from "../../hooks/useAuth";
+import { useTranslationStore } from "../../store/translationStore";
+import { LoginFormValues } from "./types";
+import validateLogin from "./validate";
+import useStyles from "./styles";
+
+const Login = () => {
+  const styles = useStyles();
+  const { intl } = useStore(useTranslationStore);
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmitForm = async (
+    values: LoginFormValues,
+    { setSubmitting, setErrors }: FormikHelpers<LoginFormValues>
+  ) => {
+    try {
+      const signInPromise = () => signIn(values.email, values.password);
+      toast.promise(signInPromise, {
+        pending: intl("loading"),
+        success: intl("loginSuccess"),
+        error: intl("loginError"),
+      });
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    } catch (error) {
+      setErrors({
+        email: intl("emailInputError"),
+        password: intl("passwordInputError"),
+      });
+    }
+    setSubmitting(false);
+  };
+
   return (
-    <div>
-      <h1>Bem-vindo à página de login!</h1>
-      <p>Esta é uma página básica de exemplo.</p>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Box sx={styles.container}>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validateLogin}
+          onSubmit={handleSubmitForm}
+        >
+          {({ handleSubmit, handleChange, values, touched, errors }) => (
+            <Form onSubmit={handleSubmit}>
+              <Field
+                as={TextField}
+                margin="normal"
+                fullWidth
+                id="email"
+                label={intl("email")}
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={values.email}
+                onChange={handleChange}
+                error={touched.email && Boolean(errors.email)}
+                helperText={touched.email && errors.email}
+                size="small"
+              />
+              <Field
+                as={TextField}
+                margin="normal"
+                fullWidth
+                name="password"
+                label={intl("password")}
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={values.password}
+                onChange={handleChange}
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+                size="small"
+              />
+
+              <Typography variant="body2" align="left">
+                <a href="/recover-password">{intl("forgotYourPassword")}</a>
+              </Typography>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                {intl("signIn")}
+              </Button>
+              <Typography variant="body2" align="center">
+                {intl("dontHaveAnAccount")}{" "}
+                <a href="/register">{intl("signUp")}</a>
+              </Typography>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </Container>
   );
 };
 
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,14 @@
+const useStyles = () => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "auto",
+    background: "white",
+    padding: "32px",
+    borderRadius: "8px",
+  },
+});
+
+export default useStyles;
